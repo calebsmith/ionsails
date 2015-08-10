f6b20257e47d0445ab8ef2edde304d40952b9f5b
Caleb Smith <caleb.smithnc@gmail.com>
2015-08-10 12:08:34 -0400

    Adds tests, main function. light refactoring of core

--- src/ionsails/core.clj
+++ src/ionsails/core.clj
@@ -1,23 +1,18 @@
-(ns ionsails.core)
+(ns ionsails.core
+  (:gen-class))
 
 (require '[lanterna.screen :as s])
 
-(def scr (s/get-screen))
 
-(def start-screen (partial s/start scr))
-(def stop-screen (partial s/stop scr))
-(def redraw-screen (partial s/redraw scr))
-(def put-screen (partial s/put-string scr))
-
-(def default-tileset {
-  :0 [ " " :black :black]
-  :1 [ " " :black :blue]
-  :2 [ "." :yellow :green]
-  :3 [ "." :black :green]
-  :4 [ "#" :yellow :green]
-  :5 [ "@" :yellow :green]
-  :6 [ "^" :yellow :green]
-})
+(def default-tileset [
+  [" " :black :black]
+  [" " :black :blue]
+  ["." :yellow :green]
+  ["." :black :green]
+  ["#" :yellow :green]
+  ["@" :yellow :green]
+  ["^" :yellow :green]
+])
 
 (def default-game-board [
  [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
@@ -35,41 +30,38 @@
  [1 1 1 1 1 1 1 1 2 1 2 2 1 1 1 1 1 1 1 1]
 ])
 
-(defn tileset-val-formatter [tileset-val]
-  (list (first tileset-val) {:fg (nth tileset-val 1) :bg (nth tileset-val 2)}))
+(defn tile-formatter [tile-val]
+    [(first tile-val) {:fg (nth tile-val 1) :bg (nth tile-val 2)}])
 
-(defn row->put-cmds [game-board tileset row-index]
-  (map-indexed (fn [index value]
-                 (concat (list index row-index)
-                         (tileset-val-formatter (get tileset (keyword (str value))))))
+(defn row->put-cmds
+    "Given a board, tileset, and a row number, return the list of arguments
+    to put-string in order to display that row"
+    [game-board tileset row-index]
+    (map-indexed (fn [index value]
+                     (concat (list index row-index)
+                             (tile-formatter (get tileset value))))
                  (nth game-board row-index)))
 
 (defn game-board->cmds [game-board]
-  (map (fn [row-index]
-         (row->put-cmds game-board default-tileset row-index))
-       (range (count game-board))))
+    (mapcat (fn [row-index]
+                (row->put-cmds game-board default-tileset row-index))
+            (range (count game-board))))
 
-(defn game-board-put [cmd-2d]
-  (doall (map (fn [cmd-lst]
-         (dorun (map (fn [cmd]
-                (apply put-screen cmd))
-         cmd-lst)))
-       cmd-2d)))
+(defn game-board-put [commands command-func]
+    (dorun (map (fn [cmd]
+                    (apply command-func cmd))
+                commands)))
 
+
+(def scr (s/get-screen))
 
 (defn render [game-board]
-  (s/clear scr)
-  (game-board-put (game-board->cmds game-board))
-  (s/move-cursor scr 0 0)
-  (redraw-screen)
-  )
+    (s/clear scr)
+    (game-board-put (game-board->cmds game-board) (partial s/put-string scr))
+    (s/move-cursor scr 0 0)
+    (s/redraw scr))
 
-
-
-; testing
-(start-screen)
-
-(render default-game-board)
-
-;(s/get-key-blocking scr)
-;(stop-screen)
+(defn -main [& args]
+  (s/in-screen scr
+    (render default-game-board)
+    (s/get-key-blocking scr)))
