4770ef2905508f91dcfff61ec301acc3af632786
Caleb Smith <caleb.smithnc@gmail.com>
2015-12-05 10:12:07 -0500

    Initial commit. Functioning figwheel project

---
+++ src/ionsails_web/core.cljs
@@ -0,0 +1,11 @@
+(ns ionsails-web.core
+  (:require ))
+
+(enable-console-print!)
+
+(println "Edits to this text should show up in your developer console for real.")
+
+(defonce app-state (atom {}))
+
+
+(defn on-js-reload [])
