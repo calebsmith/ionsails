27d0f08402df0077c1391b97f126a5fcd4aa8882
Caleb Smith <caleb.smithnc@gmail.com>
2015-12-06 22:41:08 -0500

    Fixes edge cases in trie simplifying flatten-map-keys

--- src/ionsails_web/util/trie.cljs
+++ src/ionsails_web/util/trie.cljs
@@ -3,18 +3,15 @@
 (defn- add-to-trie [trie x]
   (assoc-in trie x (merge (get-in trie x) {:terminal true})))
 
-(defn- flatten-map
+(defn- flatten-map-keys
   ([form]
-   (into {} (flatten-map form nil)))
+   (flatten-map-keys form nil))
   ([form pre]
    (mapcat (fn [[k v]]
              (let [prefix (if pre (str pre (name k)) (name k))]
-               (if (and (map? v)
-                        (not (contains? v :terminal)))
-                 (flatten-map v prefix)
-                 (if (not= k :terminal)
-                   [[prefix v]]
-                   []))))
+               (if (map? v)
+                 (flatten-map-keys v prefix)
+                 [pre])))
            form)))
 
 (defn in-trie? [trie x]
@@ -23,11 +20,8 @@
 
 (defn lookup [trie prefix]
   "Returns a list of matches with the prefix specified in the trie specified."
-  (let [sub-trie (get-in trie (vec prefix))
-        suggestions (map #(str prefix %) (keys (flatten-map sub-trie)))]
-    (if (in-trie? trie prefix)
-      (conj suggestions prefix)
-      suggestions)))
+  (let [sub-trie (get-in trie (vec prefix))]
+    (map #(str prefix %) (flatten-map-keys sub-trie))))
 
 (defn build-trie [coll]
   "Builds a trie over the values in the specified seq coll."
